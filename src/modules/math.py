78ff864a954946de246984cf3e9a34326f434c74
dw <>
2026-01-10 14:40:05 -0800

    add pitch_set function, refactor

--- src/modules/math.py
+++ src/modules/math.py
@@ -1,33 +1,37 @@
-def rotate(val, offset):
+def rotate(number, offset):
     """Circularly shift 12-digit binary numbers."""
     offset %= 12
-    return ((val >> offset) | (val << (12 - offset))) & 0xFFF
+    return ((number >> offset) | (number << (12 - offset))) & 0xFFF
 
-def cardinality(val):
-    """Compute the number of active bits in a scale value."""
-    return bin(val).count('1')
+def cardinality(number):
+    """Compute the number of active bits in a scale number."""
+    return bin(number).count('1')
 
-def reflect(val):
+def pitch_set(number):
+    """Return a list of active pitch indices (0-11) for the given scale number."""
+    return [i for i in range(12) if (number >> i) & 1]
+
+def reflect(number):
     """
     Reflect 12-bit integer about the LSB (bit 0).
     Re-orders bits: 0->0, 1->11, 2->10, ..., 11->1.
     """
-    res = val & 1
+    res = number & 1
     for i in range(1, 12):
-        if (val >> i) & 1:
+        if (number >> i) & 1:
             res |= (1 << (12 - i))
     return res
 
-def intervals(scale, n=None, direction='ascending'):
+def intervals(number, n=None, direction='ascending'):
     """
     Return the space (number of rotations) between each active bit.
     If n is given, only return the nth interval.
-    If direction is 'descending', the scale is reflected before computing intervals.
+    If direction is 'descending', the scale number is reflected before computing intervals.
     """
     if direction == 'descending':
-        scale = reflect(scale)
+        number = reflect(number)
 
-    indices = [i for i in range(12) if (scale >> i) & 1]
+    indices = pitch_set(number)
     if not indices:
         return [] if n is None else None
         
@@ -45,15 +49,15 @@ def intervals(scale, n=None, direction='ascending'):
     res.append(12 + indices[0] - indices[-1])
     return res
 
-def interval_count(val):
+def interval_count(number):
     """
     Creates a 6-digit number in base 12, where digit n is equal to the number 
-    of 1-bits in the bitwise AND of a scale value and itself shifted by n.
+    of 1-bits in the bitwise AND of a scale number and itself shifted by n.
     """
     res = 0
     for i in range(1, 7):
-        shifted = rotate(val, i)
-        count = cardinality(val & shifted)
+        shifted = rotate(number, i)
+        count = cardinality(number & shifted)
         res = (res * 12) + count
     return res
 
